bea87dd92259130491d82f1494018fb86f741776
AM-1621 <Hanawo1621@gmail.com>
2025-08-08 16:20:10 +0300

    modify

---
+++ Database/sims.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 08, 2025 at 01:19 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+DROP TABLE IF EXISTS `announcements`;
+CREATE TABLE IF NOT EXISTS `announcements` (
+  `anid` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign_parent`
+--
+
+DROP TABLE IF EXISTS `assign_parent`;
+CREATE TABLE IF NOT EXISTS `assign_parent` (
+  `apid` int NOT NULL,
+  `student_id` int NOT NULL,
+  `parent_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign_student`
+--
+
+DROP TABLE IF EXISTS `assign_student`;
+CREATE TABLE IF NOT EXISTS `assign_student` (
+  `asid` int NOT NULL,
+  `student_id` int NOT NULL,
+  `section_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign_teacher`
+--
+
+DROP TABLE IF EXISTS `assign_teacher`;
+CREATE TABLE IF NOT EXISTS `assign_teacher` (
+  `atid` int NOT NULL,
+  `teacher_id` int NOT NULL,
+  `section_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `fid` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(50) NOT NULL,
+  PRIMARY KEY (`fid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mark`
+--
+
+DROP TABLE IF EXISTS `mark`;
+CREATE TABLE IF NOT EXISTS `mark` (
+  `mid` int NOT NULL,
+  `student_id` int NOT NULL,
+  `result` int NOT NULL,
+  `semester` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_type`
+--
+
+DROP TABLE IF EXISTS `role_type`;
+CREATE TABLE IF NOT EXISTS `role_type` (
+  `rid` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  `abbreviation_name` varchar(30) NOT NULL,
+  `description` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rid`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `role_type`
+--
+
+INSERT INTO `role_type` (`rid`, `role_name`, `abbreviation_name`, `description`, `created_at`) VALUES
+(1, 'Teacher', 'TCH', 'Responsible for teaching students and managing classroom activities.', '2025-08-08 10:36:30'),
+(2, 'Admin', 'ADM', 'Handles administrative tasks and system management.', '2025-08-08 10:36:30'),
+(3, 'Director', 'DIR', 'Oversees school operations and strategic planning.', '2025-08-08 10:36:30'),
+(4, 'Instructor', 'INSTR', 'Provides instruction and support to learners.', '2025-08-08 10:36:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `tid` int NOT NULL,
+  `start_date` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE IF NOT EXISTS `section` (
+  `cid` int NOT NULL AUTO_INCREMENT,
+  `section_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `sid` int NOT NULL AUTO_INCREMENT,
+  `student_id` int NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `father_name` varchar(30) NOT NULL,
+  `grandfather_name` varchar(30) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+CREATE TABLE IF NOT EXISTS `subject` (
+  `suid` int NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`suid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `idNumber` varchar(50) NOT NULL,
+  `profile_picture` varchar(250) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `father_name` varchar(30) NOT NULL,
+  `grandfather_name` varchar(30) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `user_type` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `user_status` int NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `idNumber`, `profile_picture`, `first_name`, `father_name`, `grandfather_name`, `gender`, `user_type`, `username`, `password`, `email`, `phone`, `user_status`) VALUES
+(1, 'BSS/ADM/0001/17', '0', 'Abebe', 'Kebede', 'Zewdu', 'Male', 'Admin', 'admin', '3yFM88FVzw+XU0J2lPnUpA==', '', '912345678', 1),
+(2, 'BSS/ADM/0002/17', '../assets/img/pp.jpg', 'abenezer ', 'seyoum', 'tefera', 'M', '2', 'ab', 'nIvRPLWFfGH/pgidb0LY8A==', 'Abseyoum16@gmail.com', '+251909299398', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
