4897a11bda752f59df179d5a257023748a812440
Ebenezer Seyoum <Ebenezer-seyoum@users.noreply.github.com>
2026-07-05 08:26:29 +0300

    Prepare app for Railway deployment

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'SIMS';
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: '';
+$dbname = getenv('MYSQLDATABASE') ?: 'SIMS';
+$port = getenv('MYSQLPORT') ?: 3306;
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname, (int) $port);
 if(!$conn){
     die('Connection failed: '.mysqli_connect_error());
 }/*else{
